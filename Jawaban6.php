4c847184ae0391efe9fb8325f1a5d7d7419a5c97
yoongi's future wife <130167211+dyahnurmala@users.noreply.github.com>
2023-11-16 21:07:15 +0700

    "first commit"

--- Jawaban6.php
+++ Jawaban6.php
@@ -1,11 +1,11 @@
-#########################################################################################
+############################################################################################
 Koneksi database adalah hubungan logis antara aplikasi dan database yang memungkinkan 
 aplikasi mengirim pertanyaan dan perintah, serta menerima data dan hasil. Untuk membuat 
 koneksi database, perlu memberikan beberapa informasi, seperti nama database, host, port, 
 nama pengguna, kata sandi, dan driver. Driver adalah komponen perangkat lunak yang 
 memungkinkan aplikasi berkomunikasi dengan sistem basis data tertentu, 
 seperti MySQL, Oracle, atau MongoDB.
-#########################################################################################
+############################################################################################
 
 <?php
 $servername = "localhost";
