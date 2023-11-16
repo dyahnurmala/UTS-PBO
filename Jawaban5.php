30fd67f24322f7046398f73b39d45128a7834e25
yoongi's future wife <130167211+dyahnurmala@users.noreply.github.com>
2023-11-16 20:08:15 +0700

    "first commit"

---
+++ Jawaban5.php
@@ -0,0 +1,25 @@
+#########################################################################################
+Dasar Teori GUI (Graphical User Interface) adalah suatu model antarmuka komputer yang 
+menggunakan objek-objek grafis sebagai media interaksi dengan user.konsep antarmuka 
+berbasis grafis yang dikembangkan untuk menggantikan antarmuka berbasis teks. 
+#########################################################################################
+
+<?php   
+interface MyInterfaceName{
+    public function metodeA();
+    public function metodeB();
+} 
+
+class MyClassName implements MyInterfaceName{
+    public function metodeA(){
+        echo "MetodeA Called" . "\n";
+    } 
+    public function metodeB(){
+        echo "MetodeB Called". "\n";
+    }
+} 
+
+$obj = new MyClassName;
+$obj->metodeA();
+$obj->metodeB();
+?>
---
+++ Jawaban6.php
@@ -0,0 +1,22 @@
+#########################################################################################
+Koneksi database adalah hubungan logis antara aplikasi dan database yang memungkinkan 
+aplikasi mengirim pertanyaan dan perintah, serta menerima data dan hasil. Untuk membuat 
+koneksi database, perlu memberikan beberapa informasi, seperti nama database, host, port, 
+nama pengguna, kata sandi, dan driver. Driver adalah komponen perangkat lunak yang 
+memungkinkan aplikasi berkomunikasi dengan sistem basis data tertentu, 
+seperti MySQL, Oracle, atau MongoDB.
+#########################################################################################
+
+
+APP_NAME=Laravel
+APP_ENV=local
+APP_KEY=base64:4Mg7nT8AiLOOe0u8t5Wnq4TzR/cB1CdygBZ6pnuDaU8=
+APP_DEBUG=true
+APP_URL=http://localhost
+
+DB_CONNECTION=mysql
+DB_HOST=172.28.160.152
+DB_PORT=23306
+DB_DATABASE=dyah
+DB_USERNAME=root
+DB_PASSWORD=p455w0rd
